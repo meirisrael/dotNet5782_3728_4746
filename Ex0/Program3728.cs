9c94dda15c5433a6e1570b71563eeecfe02f4e8d
meir ariche <meirisrael770@gmail.com>
2021-10-11 11:50:15 +0300

    initial code

---
+++ Ex0/Program3728.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex0
+{
+	partial class Program
+	{
+		static void Main(string[] args)
+		{
+			welcome3728();
+			welcome4746();
+
+			Console.ReadKey();
+		}
+
+		static partial void welcome4746();
+		private static void welcome3728()
+		{
+			Console.Write("Enter your name: ");
+			string name = Console.ReadLine();
+			Console.WriteLine($"{name}, welcome to my first console application");
+		}
+	}
+}
