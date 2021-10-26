25485bfadce66ea42645a4b406c70a3db83c39d3
meirisrael <meirisrael770@gmail.com>
2021-10-26 23:39:03 +0300

    Ex1-last version

--- DAL/Customer.cs
+++ DAL/Customer.cs
@@ -13,7 +13,7 @@ namespace IDAL
 		/// </summary>
 		public struct Customer
 		{
-			public int Id { get; set; }
+			public int Id { set; get; }
 			public string Name { get; set; }
 			public string Phone { get; set; }
 			public double Longitude { get; set; }
