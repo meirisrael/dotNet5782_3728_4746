6ae3f2c3e814205c48c8b6961fa103fcdc7e2fbb
meirisrael <meirisrael770@gmail.com>
2021-12-12 20:21:53 +0200

    Last commit

--- PL/displayListOfDrones.xaml.cs
+++ PL/displayListOfDrones.xaml.cs
@@ -67,36 +67,14 @@ namespace PL
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
 		private void StatusSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			if (StatusSelector.SelectedItem == null && WeightSelector.ItemsSource == null)
-				drones = drones;
-			else if (StatusSelector.SelectedItem != null && WeightSelector.SelectedItem == null)
-				drones = bl.GetListOfDrones(d => d.Status == (IBL.BO.DroneStatuses)StatusSelector.SelectedItem);
-			else if (StatusSelector.SelectedItem != null && WeightSelector.SelectedItem != null)
-				drones = bl.GetListOfDrones(d => d.Status == (IBL.BO.DroneStatuses)StatusSelector.SelectedItem && d.MaxWeight == (IBL.BO.WeightCategories)WeightSelector.SelectedItem);
-			else
-				drones = bl.GetListOfDrones(d => true);
-
-			DroneListView.ItemsSource = drones;
-		}
+		{ filterByStatus(); }
 		/// <summary>
 		/// if the user select a weight to filter
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
 		private void WeightSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			if (WeightSelector.SelectedItem == null && StatusSelector.SelectedItem == null)
-				drones = drones;
-			else if (WeightSelector.SelectedItem != null && StatusSelector.SelectedItem == null)
-				drones = bl.GetListOfDrones(d => d.MaxWeight == (IBL.BO.WeightCategories)WeightSelector.SelectedItem);
-			else if (WeightSelector.SelectedItem != null && StatusSelector.SelectedItem != null)
-				drones = bl.GetListOfDrones(d => d.Status == (IBL.BO.DroneStatuses)StatusSelector.SelectedItem && d.MaxWeight == (IBL.BO.WeightCategories)WeightSelector.SelectedItem);
-			else
-				drones = bl.GetListOfDrones(d => d.Status == (IBL.BO.DroneStatuses)StatusSelector.SelectedItem);
-
-			DroneListView.ItemsSource = drones;
-		}
+		{ filterByWeight(); }
 
 		/// <summary>
 		/// if the user press the button clear "status selector"
@@ -136,7 +114,9 @@ namespace PL
 		/// <param name="e"></param>
 		private void Add_Click(object sender, RoutedEventArgs e)
 		{
-			new droneWindow(bl,DroneListView).Show();
+			new droneWindow(bl, DroneListView).ShowDialog();
+			filterByStatus();
+			filterByWeight();
 		}
 		/// <summary>
 		/// if the user do a double click on the drone list view
@@ -158,6 +138,39 @@ namespace PL
 			
 		}
 
+		/// <summary>
+		/// filter the list view by the status of drone
+		/// </summary>
+		private void filterByStatus()
+		{
+			if (StatusSelector.SelectedItem == null && WeightSelector.ItemsSource == null)
+				drones = drones;
+			else if (StatusSelector.SelectedItem != null && WeightSelector.SelectedItem == null)
+				drones = bl.GetListOfDrones(d => d.Status == (IBL.BO.DroneStatuses)StatusSelector.SelectedItem);
+			else if (StatusSelector.SelectedItem != null && WeightSelector.SelectedItem != null)
+				drones = bl.GetListOfDrones(d => d.Status == (IBL.BO.DroneStatuses)StatusSelector.SelectedItem && d.MaxWeight == (IBL.BO.WeightCategories)WeightSelector.SelectedItem);
+			else
+				drones = bl.GetListOfDrones(d => true);
+
+			DroneListView.ItemsSource = drones;
+		}
+		/// <summary>
+		/// filter the list view by the weight of drone
+		/// </summary>
+		private void filterByWeight()
+		{
+
+			if (WeightSelector.SelectedItem == null && StatusSelector.SelectedItem == null)
+				drones = drones;
+			else if (WeightSelector.SelectedItem != null && StatusSelector.SelectedItem == null)
+				drones = bl.GetListOfDrones(d => d.MaxWeight == (IBL.BO.WeightCategories)WeightSelector.SelectedItem);
+			else if (WeightSelector.SelectedItem != null && StatusSelector.SelectedItem != null)
+				drones = bl.GetListOfDrones(d => d.Status == (IBL.BO.DroneStatuses)StatusSelector.SelectedItem && d.MaxWeight == (IBL.BO.WeightCategories)WeightSelector.SelectedItem);
+			else
+				drones = bl.GetListOfDrones(d => d.Status == (IBL.BO.DroneStatuses)StatusSelector.SelectedItem);
+
+			DroneListView.ItemsSource = drones;
+		}
 		/// <summary>
 		/// if the user press the button close so close the window
 		/// </summary>
