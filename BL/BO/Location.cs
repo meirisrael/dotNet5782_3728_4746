91d447e945bd9a6cc9b3e7c478338ef2387a56a6
meirisrael <meirisrael770@gmail.com>
2022-01-14 15:18:48 +0200

    loc

--- BL/BO/Location.cs
+++ BL/BO/Location.cs
@@ -13,12 +13,12 @@ namespace BO
 
 		public override string ToString()
 		{
-			string lati = $"{Math.Abs((int)(this.Latitude))}°{Math.Abs((int)(((this.Latitude) - (int)(this.Latitude)) * 60))}'{Math.Abs(Math.Round(((((this.Latitude) - (int)(this.Latitude)) * 60) - (int)(((this.Latitude) - (int)(this.Latitude)) * 60)) * 60, 3))}''";
+			string lati = $"{Math.Abs((int)(this.Latitude))}°{Math.Abs((int)(((this.Latitude) - (int)(this.Latitude)) * 60))}'{Math.Abs(Math.Round(((((this.Latitude) - (int)(this.Latitude)) * 60) - (int)(((this.Latitude) - (int)(this.Latitude)) * 60)) * 60, 5))}''";
 			if (Latitude < 0)
 				lati += 'W';
 			else
 				lati += 'E';
-			string longi = $"{ Math.Abs((int)(this.Longitude)) }°{ Math.Abs((int)(((this.Longitude) - (int)(this.Longitude)) * 60))}'{Math.Abs(Math.Round(((((this.Longitude) - (int)(this.Longitude)) * 60) - (int)(((this.Longitude) - (int)(this.Longitude)) * 60)) * 60, 3))}''";
+			string longi = $"{ Math.Abs((int)(this.Longitude)) }°{ Math.Abs((int)(((this.Longitude) - (int)(this.Longitude)) * 60))}'{Math.Abs(Math.Round(((((this.Longitude) - (int)(this.Longitude)) * 60) - (int)(((this.Longitude) - (int)(this.Longitude)) * 60)) * 60, 5))}''";
 			if (Longitude < 0)
 				longi += 'S';
 			else
